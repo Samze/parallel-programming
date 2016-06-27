6e610788a3f171609b01049a4fe76fa210cf3352
sam gunaratne <samgzeit@gmail.com>
2016-06-27 12:06:21 +0100

    Simple concurrency

--- main.go
+++ main.go
@@ -23,11 +23,32 @@ func McCount(iter int) int {
 	return hits
 }
 
+func EstimatePiConcurrently(iter, routines int) float64 {
+	hitsChan := make(chan int)
+
+	iterPerRoutine := iter / routines
+
+	for i := 0; i < routines; i++ {
+		go func() {
+			hitsChan <- McCount(iterPerRoutine)
+		}()
+	}
+
+	hits := 0
+	for i := 0; i < routines; i++ {
+		hits = hits + <-hitsChan
+	}
+
+	return 4.0 * float64(hits) / float64(iter)
+}
+
 func EstimatePiSequentially(iter int) float64 {
 	return 4.0 * float64(McCount(iter)) / float64(iter)
 }
 
 func main() {
-	pi := EstimatePiSequentially(1 << 20)
-	fmt.Printf("%f", pi)
+	piSeq := EstimatePiSequentially(1 << 25)
+	piPar := EstimatePiConcurrently(1<<25, 4)
+	fmt.Println(fmt.Sprintf("seq %f", piSeq))
+	fmt.Println(fmt.Sprintf("par %f", piPar))
 }
